05055c6403e71473b7d34b576f91b1672f3b7c9c
Strooom <pascal.roobrouck@gmail.com>
2020-12-06 13:10:08 +0100

    starting getNextStep

--- Software/motionctrl.cpp
+++ Software/motionctrl.cpp
@@ -158,123 +158,123 @@ step motionCtrl::getNextStep() {
     // temp code : needs to return something to make the compiler happy
     return step{0, 0};
 
-    //while (true)
-    //    {
-    //    timeInMotionTicks++;																// Determine sampling time as 'previous sample time' + sampling interval (1)
-    //    lastReload++;
-    //    while (true)
-    //        {
-    //        if (motionBufferLevel > 0)
-    //            {
-    //            motion* currentMotionItem = &motionBuffer[motionBufferReadIndex];		// pointer to active motion, speeds up accessing its members
-    //            float timeInMotionFloat;													// equivalent in floating point seconds
-    //            timeInMotionFloat = (float)timeInMotionTicks * timerTick;					// convert discrete time in timerticks to float time in seconds
+    while (true)
+        {
+        timeInMotionTicks++;																// Determine sampling time as 'previous sample time' + sampling interval (1)
+        lastReload++;
+        while (true)
+            {
+            if (motionBufferLevel > 0)
+                {
+                motion* currentMotionItem = &motionBuffer[motionBufferReadIndex];		// pointer to active motion, speeds up accessing its members
+                float timeInMotionFloat;													// equivalent in floating point seconds
+                timeInMotionFloat = (float)timeInMotionTicks * timerTick;					// convert discrete time in timerticks to float time in seconds
 
-    //            if (timeInMotionFloat > currentMotionItem->tMotion)
-    //                {
-    //                timeInMotionTicks = timeInMotionTicks - (uint32_t)((currentMotionItem->tMotion) * timerFrequency);
-    //                //timeInMotionFloat = timeInMotionFloat - currentMotionItem->tMotion;
+                if (timeInMotionFloat > currentMotionItem->tMotion)
+                    {
+                    timeInMotionTicks = timeInMotionTicks - (uint32_t)((currentMotionItem->tMotion) * timerFrequency);
+                    //timeInMotionFloat = timeInMotionFloat - currentMotionItem->tMotion;
 
-    //                motionBufferReadIndex = (motionBufferReadIndex + 1) % motionBufferLength;
-    //                --motionBufferLevel;
+                    motionBufferReadIndex = (motionBufferReadIndex + 1) % motionBufferLength;
+                    --motionBufferLevel;
 
-    //                //pushEvent(event::limitSwitchXMaxOpened);	//fire a motionComplete event();
-    //                continue;
-    //                }
-    //            else
-    //                {
-    //                float sNow = currentMotionItem->s(timeInMotionFloat);
+                    //pushEvent(event::limitSwitchXMaxOpened);	//fire a motionComplete event();
+                    continue;
+                    }
+                else
+                    {
+                    float sNow = currentMotionItem->s(timeInMotionFloat);
 
-    //                uint32_t mask = 0x00000001;	// initialize bitmask needed for manipulating Step and Dir bits
-    //                // 4. For each axis..
-    //                for (uint8_t i = 0; i < (uint8_t)axis::nmbrAxis; ++i)
-    //                    {
-    //                    // 4a. Determine the position for this axis for this distance travelled
-    //                    if (currentMotionItem->deltaRealTime[i] != 0.0F)			// Optimization : only do calculations for Axis that are moving
-    //                        {
-    //                        if (i == (uint8_t)currentMotionItem->arcAxis0)
-    //                            {
-    //                            nextPositionInMm[i] = (currentMotionItem->arcCenter0 + (currentMotionItem->radius * cosf(currentMotionItem->startAngle + (currentMotionItem->deltaRealTime[i] * sNow))));
-    //                            }
-    //                        else if (i == (uint8_t)currentMotionItem->arcAxis1)
-    //                            {
-    //                            nextPositionInMm[i] = (currentMotionItem->arcCenter1 + (currentMotionItem->radius * sinf(currentMotionItem->startAngle + (currentMotionItem->deltaRealTime[i] * sNow))));
-    //                            }
-    //                        else
-    //                            {
-    //                            nextPositionInMm[i] = (currentMotionItem->startPosition[i] + currentMotionItem->deltaRealTime[i] * sNow);
-    //                            }
+                    uint32_t mask = 0x00000001;	// initialize bitmask needed for manipulating Step and Dir bits
+                    // 4. For each axis..
+                    for (uint8_t i = 0; i < (uint8_t)axis::nmbrAxis; ++i)
+                        {
+                        // 4a. Determine the position for this axis for this distance travelled
+                        if (currentMotionItem->deltaRealTime[i] != 0.0F)			// Optimization : only do calculations for Axis that are moving
+                            {
+                            if (i == (uint8_t)currentMotionItem->arcAxis0)
+                                {
+                                nextPositionInMm[i] = (currentMotionItem->arcCenter0 + (currentMotionItem->radius * cosf(currentMotionItem->startAngle + (currentMotionItem->deltaRealTime[i] * sNow))));
+                                }
+                            else if (i == (uint8_t)currentMotionItem->arcAxis1)
+                                {
+                                nextPositionInMm[i] = (currentMotionItem->arcCenter1 + (currentMotionItem->radius * sinf(currentMotionItem->startAngle + (currentMotionItem->deltaRealTime[i] * sNow))));
+                                }
+                            else
+                                {
+                                nextPositionInMm[i] = (currentMotionItem->startPosition[i] + currentMotionItem->deltaRealTime[i] * sNow);
+                                }
 
-    //                        // 4b. Determine if this new position requires a step, forward or backwards
-    //                        // NOTE - TODO there is some problem here as the conversion from float to int rounds towards zero (discards the fractional part) which means the interval [-1,1] takes twice the time..
-    //                        // Should test this on the K64/K66 to see how it behaves there
-    //                        if ((int32_t)((nextPositionInMm[i] * theMachineProperties.motors.stepsPerMm[i]) - hysteresis) > currentPositionInSteps[i])         // Determine if this position involves step forward...
-    //                            {
-    //                            // Step forward
-    //                            //logger.logNow("Step++\n");
-    //                            dirSetup = dirSetup & ~mask;			// mask step signal to zero (should already be zero)
-    //                            stepRise = stepRise | mask;				// set step pulse high
-    //                            stepFall = stepFall & ~mask;			// return step pulse low
-    //                            mask = mask << 1;						// adjust mask for direction bits
-    //                            dirSetup = dirSetup | mask;				// set direction high
-    //                            stepRise = stepRise | mask;				// set direction high
-    //                            stepFall = stepFall | mask;				// set direction high
-    //                            mask = mask << 1;						// adjust mask for next iteration = next axis
-    //                            currentPositionInSteps[i]++;
-    //                            }
-    //                        else if ((int32_t)((nextPositionInMm[i] * theMachineProperties.motors.stepsPerMm[i]) + hysteresis) < currentPositionInSteps[i])		// ... or a step backwards
-    //                            {
-    //                            // Step backwards
-    //                            dirSetup = dirSetup & ~mask;			// mask step signal to zero (should already be zero)
-    //                            stepRise = stepRise | mask;				// set step pulse high
-    //                            stepFall = stepFall & ~mask;			// return step pulse low
-    //                            mask = mask << 1;						// adjust mask for direction bits
-    //                            dirSetup = dirSetup & ~mask;			// set direction low
-    //                            stepRise = stepRise & ~mask;			// set direction low
-    //                            stepFall = stepFall & ~mask;			// set direction low
-    //                            mask = mask << 1;						// adjust mask for next iteration = next axis
-    //                            currentPositionInSteps[i]--;
-    //                            }
-    //                        else
-    //                            {
-    //                            mask = mask << 2;
-    //                            }
-    //                        }
-    //                    else
-    //                        {
-    //                        mask = mask << 2;
-    //                        }
-    //                    }
-    //                if ((dirSetup != lastOut) || (lastReload >= defaultReload))
-    //                    {
-    //                    lastOut = dirSetup;
-    //                    stepperItem output;
-    //                    output.out = dirSetup;
-    //                    output.reload = lastReload-1;
-    //                    lastReload = 0;
+                            // 4b. Determine if this new position requires a step, forward or backwards
+                            // NOTE - TODO there is some problem here as the conversion from float to int rounds towards zero (discards the fractional part) which means the interval [-1,1] takes twice the time..
+                            // Should test this on the K64/K66 to see how it behaves there
+                            if ((int32_t)((nextPositionInMm[i] * theMachineProperties.motors.stepsPerMm[i]) - hysteresis) > currentPositionInSteps[i])         // Determine if this position involves step forward...
+                                {
+                                // Step forward
+                                //logger.logNow("Step++\n");
+                                dirSetup = dirSetup & ~mask;			// mask step signal to zero (should already be zero)
+                                stepRise = stepRise | mask;				// set step pulse high
+                                stepFall = stepFall & ~mask;			// return step pulse low
+                                mask = mask << 1;						// adjust mask for direction bits
+                                dirSetup = dirSetup | mask;				// set direction high
+                                stepRise = stepRise | mask;				// set direction high
+                                stepFall = stepFall | mask;				// set direction high
+                                mask = mask << 1;						// adjust mask for next iteration = next axis
+                                currentPositionInSteps[i]++;
+                                }
+                            else if ((int32_t)((nextPositionInMm[i] * theMachineProperties.motors.stepsPerMm[i]) + hysteresis) < currentPositionInSteps[i])		// ... or a step backwards
+                                {
+                                // Step backwards
+                                dirSetup = dirSetup & ~mask;			// mask step signal to zero (should already be zero)
+                                stepRise = stepRise | mask;				// set step pulse high
+                                stepFall = stepFall & ~mask;			// return step pulse low
+                                mask = mask << 1;						// adjust mask for direction bits
+                                dirSetup = dirSetup & ~mask;			// set direction low
+                                stepRise = stepRise & ~mask;			// set direction low
+                                stepFall = stepFall & ~mask;			// set direction low
+                                mask = mask << 1;						// adjust mask for next iteration = next axis
+                                currentPositionInSteps[i]--;
+                                }
+                            else
+                                {
+                                mask = mask << 2;
+                                }
+                            }
+                        else
+                            {
+                            mask = mask << 2;
+                            }
+                        }
+                    if ((dirSetup != lastOut) || (lastReload >= defaultReload))
+                        {
+                        lastOut = dirSetup;
+                        stepperItem output;
+                        output.out = dirSetup;
+                        output.reload = lastReload-1;
+                        lastReload = 0;
 
-    //                    // Shift the bitpatterns for the next run..
-    //                    dirSetup = stepRise;
-    //                    stepRise = stepFall;
-    //                    return output;
-    //                    }
-    //                }
-    //            }
-    //        else
-    //            {
-    //            // no more motionItems in the buffer -> repeat whatever is already written out
-    //            timeInMotionTicks = 0;
-    //            stepperItem output;
-    //            output.out = lastOut;
-    //            output.reload = defaultReload-1;
-    //            return output;
-    //            }
-    //        break;
-    //        }
-    //    // shift the bitpatterns for the next run..
-    //    dirSetup = stepRise;
-    //    stepRise = stepFall;
-    //    }
+                        // Shift the bitpatterns for the next run..
+                        dirSetup = stepRise;
+                        stepRise = stepFall;
+                        return output;
+                        }
+                    }
+                }
+            else
+                {
+                // no more motionItems in the buffer -> repeat whatever is already written out
+                timeInMotionTicks = 0;
+                stepperItem output;
+                output.out = lastOut;
+                output.reload = defaultReload-1;
+                return output;
+                }
+            break;
+            }
+        // shift the bitpatterns for the next run..
+        dirSetup = stepRise;
+        stepRise = stepFall;
+        }
 }
 
 void motionCtrl::run() {
